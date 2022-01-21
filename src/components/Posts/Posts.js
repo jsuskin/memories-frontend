9d68a9e86e9abfb75cf84c2593ddb2a2cce39a6e
jsuskin <jsuskin85@gmail.com>
2022-01-21 15:40:25 -0500

    first commit

---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,25 @@
+import Post from "./Post/Post";
+import { useSelector } from "react-redux";
+import { Grid, CircularProgress } from "@material-ui/core";
+import useStyles from "./styles";
+
+function Posts({ setCurrentId }) {
+  const posts = useSelector((state) => state.posts);
+  const classes = useStyles();
+
+  console.log(posts);
+
+  return !posts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid container className={classes.container} alignItems='stretch' spacing={3}>
+      {posts.map((post) => (
+        <Grid item key={post._id} xs={12} sm={6}>
+          <Post post={post} setCurrentId={setCurrentId} />
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
+
+export default Posts;
